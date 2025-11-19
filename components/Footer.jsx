9f6a423631c5af912eb6ab60c7314e3c98e14c36
dovelk <dovelkaim0@gmail.com>
2025-11-19 12:55:57 +0200

    ikcen

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,11 @@
-// Footer.jsx
 
 import React from 'react';
-// 1. Changez l'importation pour la famille Font Awesome (Fa)
 import { FaFacebook, FaTwitter, FaGithub } from "react-icons/fa"; 
 
 function Footer() {
   return (
     <footer className="site-footer">
       <div className="social-icons">
-        {/* 2. Utilisez les nouvelles icônes Fa */}
         <FaFacebook size={30} color="#1877F2" />
         <FaTwitter size={30} color="#1DA1F2" />
         <FaGithub size={30} color="#000" />
