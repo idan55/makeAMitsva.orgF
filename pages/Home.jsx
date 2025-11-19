7223d3a24288f3564f7c0f15b7bf002a8a45fc3e
dovelk <dovelkaim0@gmail.com>
2025-11-19 12:42:18 +0200

    hey mon coeur

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,31 +1,19 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Map from "../components/Map";
+import React, { useEffect } from 'react'
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
 function Home() {
-    return (
-      <div className="min-h-screen bg-gray-50 flex flex-col items-center p-4 font-sans">
-        <Header/>
-        <main className="w-full max-w-4xl bg-white shadow-2xl rounded-xl p-8 my-8">
-          
-          {/* Titre et description de l'application */}
-          <div className="text-center mb-8">
-              <h1 className="text-4xl font-extrabold text-blue-700 mb-2">
-                  Mon Localisateur Simple
-              </h1>
-              <p className="text-gray-600">
-                  Affiche votre emplacement sur une carte simulée. L'application demande l'autorisation de géolocalisation.
-              </p>
-          </div>
-          
-          {/* Conteneur de la carte */}
-          <div className="w-full h-auto">
-              <Map />
-          </div>
-          
-        </main>
-  
+
+  return (
+    <div className="page-container">
+      <Header />
+      <div className="content">
+        <h1>Home Page</h1>
       </div>
-    );
-  }
-  
-  export default Home;
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
+
